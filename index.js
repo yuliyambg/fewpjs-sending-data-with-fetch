194faa0aea5072f41d8fcf3a4b89ad2941a86e22
yuliyambg <yuliyambg@gmail.com>
2021-02-15 16:04:13 -0500

    commit tree 8eb67cf12fbd0eac386d68eb15cbeb69b4b92575

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+// alert("going to create new dog");
+function submitData(name, email) {
+return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify({
+        name,
+        email
+    })
+})
+    .then(resp => resp.json())
+    .then(json => {
+        document.body.innerHTML = json["id"]
+    })
+    .catch(error => {
+        document.body.innerHTML = error.message
+    })
+}
