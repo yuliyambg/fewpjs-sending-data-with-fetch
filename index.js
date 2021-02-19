f455d7ab31fc03721e68e4ef8873225d6b6e7ef1
yuliyambg <yuliyambg@gmail.com>
2021-02-19 18:03:20 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+// alert("going to create new dog");
+function submitData(name, email) {
+return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify({
+        name,
+        email
+    })
+})
+    .then(resp => resp.json())
+    .then(json => {
+        console.log(json)
+        document.body.innerHTML = json["id"]
+    })
+    .catch(error => {
+        document.body.innerHTML = error.message
+    })
+}
